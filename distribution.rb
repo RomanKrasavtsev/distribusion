44fdafacfc06f88390a6c9d4e2b4d307cd72438b
Roman Krasavtsev <mr.krasavtsev@gmail.com>
2017-05-09 17:21:17 +0200

    add methods for getting routes

--- distribution.rb
+++ distribution.rb
@@ -2,12 +2,19 @@
 
 require 'net/http'
 require 'json'
+require 'zip'
 
 class Matrix
   START_URL = "http://challenge.distribusion.com/the_one"
+  ROUTES_URL = "http://challenge.distribusion.com/the_one/routes"
 
   def initialize
     red_pill = take_a_red_pill
+    @passphrase = red_pill["passphrase"]
+    @location = red_pill["location"]
+    @source_type = "sentinels"
+
+    sentinels_routes = routes
   end
 
   private
@@ -24,6 +31,43 @@ class Matrix
     if response.content_type == 'application/json'
       JSON.parse(response.body.force_encoding('UTF-8'))["pills"]["red"]
     end
+  end
+
+  def routes
+    uri = URI(ROUTES_URL)
+    uri.query = URI.encode_www_form({
+      passphrase: @passphrase,
+      source: @source_type
+    })
+
+    response = Net::HTTP.get_response(uri)
+
+    if response.content_type == 'application/zip'
+      files = unzip_responce(response)
+      files[0][:content] if files.count == 1
+    end
+  end
+
+  def unzip_responce(response)
+    file_name = response["content-disposition"].gsub(/attachment; filename\*=UTF-8''/, '') + ".zip"
+    file = File.open(file_name, "w")
+    file.write(response.body)
+    file.close
+
+    content = []
+    Zip::File.open(file.path) do |zip_file|
+      zip_file.each do |entry|
+        if entry.ftype == :file && entry.name != "__MACOSX/sentinels/._routes.csv"
+          item = {}
+          item[:file] = entry.name
+          item[:content] = entry.get_input_stream.read
+          content << item
+        end
+      end
+    end
+
+    File.delete(file.path)
+    content
   end
 end
 
